05d286abdba0067a0c60e31195a212fab48ce09a
BlueBlood-dev <62560825+BlueBlood-dev@users.noreply.github.com>
2020-05-08 23:33:35 +0300

    Summary

--- SimpleGui/main.py
+++ SimpleGui/main.py
@@ -1,32 +1,42 @@
-from tkinter import *
+import tkinter as tk
+from PIL import ImageTk, Image
 
-myWindow = Tk()
+root = tk.Tk()
 
-def closeMywindow():
-    myWindow.quit()
-
-def throwToTableMenu():
-    closeMywindow()
-    tableMenu = Tk()
-    tableMenu.title('Edit your table')
-    tableMenu.configure(bg='white')
-    tableMenu.mainloop()
+def setGeometry():
+    root.resizable(False, False)
+    windowWidth = root.winfo_reqwidth()
+    windowHeight = root.winfo_reqheight()
+    positionRight = int(root.winfo_screenwidth()/2.5 - windowWidth/2)
+    positionDown = int(root.winfo_screenheight()/3.5 - windowHeight/2)
+    root.geometry('+{}+{}'.format(positionRight, positionDown))
 
 def getStart():
-    myWindow.title('Table creator')
-    myWindow.configure(bg='#c1d5d2')
-    Label(myWindow, text='Press the button below to start creating your table',
-                font=("TimesNewRoman", 13), bg='#c1d5d2').grid(column = 5 , row = 5, sticky= 'e')
+    root.title('TableMaker')
+    root.configure(bg='#ffcccc')
+    frame1 = tk.Frame(master=root, relief=tk.FLAT,
+                      borderwidth=50, bg='#ffccff')
+    helloText = tk.Label(master=frame1, bg='#ffccff',
+                         text='Hi, this is a simple TableMaker app', font=('TimesNewRoman', 20))
+    helloText.pack()
+    frame1.pack(fill=tk.BOTH, side=tk.TOP)
+    ButtonFrame = tk.Frame(master=root, relief=tk.FLAT, bg='#ff8080', borderwidth=20)
+    tk.Button(master=ButtonFrame, relief=tk.RAISED, text='Press me to start', bg='#ff8080', fg='#000099',
+                                width=30, font=('TimesNewRoman', 25), command = throwToTableMenu).pack()
+    ButtonFrame.pack(fill = tk.BOTH, side = tk.TOP)
+    ImageFrame = tk.Frame(master=root, relief= tk.FLAT, borderwidth = 70)
+    img = ImageTk.PhotoImage(Image.open('heart22.png'))
+    tk.Label(master = ImageFrame, image = img ).pack()
+    ImageFrame.pack(fill=tk.BOTH, side=tk.TOP)
+    setGeometry()
+    root.mainloop()
 
-    Button(myWindow, text='Создать таблицу', bg='#808080',
-                fg='#000000',command = throwToTableMenu).grid(column=5)
 
-    Button(myWindow, text='quit',  bg='#808080',
-        fg='#000000', command=closeMywindow).grid(column=100, row=100)
-    myWindow.mainloop()
+def setMainFrames():
+    pass
+
+def throwToTableMenu():
+    pass
 
 
 getStart()
-
-
-
